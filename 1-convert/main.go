c47d211c53e108ecc48a71aa8c1a08405cd20f9f
AlekseyPetrik <Alexey.Petric@gmail.com>
2024-07-08 21:20:21 +1000

    converter funcs

--- 1-convert/main.go
+++ 1-convert/main.go
@@ -2,9 +2,20 @@ package main
 
 import "fmt"
 
+func input() string {
+	var line string
+	fmt.Print("Введите значение: ")
+	fmt.Scan(&line)
+	return line
+}
+
+func calc(a, b, c float64) float64 {
+	return 0
+}
+
 func main() {
 	const UsdToEur = 0.92305
 	const UsdToRub = 0.011346
-
-	fmt.Println("EurToRub-", UsdToEur*UsdToRub)
+	input()
+	fmt.Println("EurToRub -", UsdToEur*UsdToRub)
 }
